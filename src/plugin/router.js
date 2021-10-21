5ddce34610ffe5efad16ff4df9b38b7c38cbec38
SARA <sara.polese.9092@gmail.com>
2021-10-21 11:15:10 +0200

    fix: #4
    added router  all pages

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -3,17 +3,35 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 //Pages
-import Home from "../pages/Home";
-import Contact from "../pages/Contact";
+import Home from "../pages/Home.vue";
+import Manga from "../pages/Manga.vue";
+import Contact from "../pages/AboutUs.vue";
+import AboutUs from "../pages/Contact.vue";
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/contact', component: Contact }
+    { path: '/', 
+      component: Home
+    },
+
+    { path: '/Manga', 
+      component: Manga
+    },
+
+    { path: '/AboutUs', 
+      component: AboutUs
+    },
+
+    { path: '/Contact', 
+      component: Contact
+    },
 ]
+
+
+
 
 const router = new VueRouter({
     mode: 'history',
     routes: routes // short for `routes: routes`
-})
+}) 
 
 export default router
